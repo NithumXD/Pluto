f97703af6b73fe22b731484f5f77a393bd825d75
NithumXD <nithumthiranja2@gmail.com>
2024-09-09 18:19:19 +0530

    Add files via upload

---
+++ Pluto/signup.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="Resources/Pluto.ico" type="image/x-icon">
+    <link rel="stylesheet" href="w3.css">
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Sign Up</title>
+</head>
+<body class="body">
+<div class="card col-6 offset-3">
+    <div class="card-body">
+        
+    </div>
+</div>
+<script src="script.js"></script>   
+</body>
+</html>
